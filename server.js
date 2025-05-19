7b0d66e97ff55439da8d8a0f1e996e8010c8c686
Norrheden <eliasnorrhedenmau@gmail.com>
2025-05-19 16:12:07 +0200

    headerCors

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 async function handler(request) {
     let url = new URL(request.url);
 
+
+    const headersCors = new Headers();
+    headersCors.set("Content-Type", "application/json")
+    headersCors.set("access-control-allow-origin", "*");
+
     if(url.pathname = "/create") {
         if(request.method === "POST") {
             let resource = await request.json();
             console.log(resource)
             let response = new Response(null, {
-                status: 200
+                status: 200,
+                headers: headersCors
             })
             return response;
         }
