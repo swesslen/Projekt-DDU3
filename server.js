9b4309ca24165bed438894711deb64d4e7e554d6
Norrheden <eliasnorrhedenmau@gmail.com>
2025-05-19 16:30:24 +0200

    added OPTION method to server

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 async function handler(request) {
     let url = new URL(request.url);
+    console.log(request)
 
 
     const headersCors = new Headers();
     headersCors.set("Content-Type", "application/json")
     headersCors.set("access-control-allow-origin", "*");
-
+    
     if(url.pathname === "/create") {
+        console.log("enfw")
+        if (request.method === "OPTIONS") { // Måste ha annars funkar ej
+            headersCors.set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+            headersCors.set("Access-Control-Allow-Headers", "Content-Type");
+            return new Response(null, {
+              status: 204,
+              headers: headersCors
+            });
+        }
         if(request.method === "POST") {
-            let resource = await request.json();
+            let resource = await request.json(); // { name: "test", password: "123" } t.ex
             console.log(resource)
             let response = new Response(null, {
                 status: 200,
