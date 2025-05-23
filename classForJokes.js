fcb3fc5fdef4ce0bb4e8a5f6d18e96e04c7db461
swesslen <simon@jolowe.se>
2025-05-23 11:07:56 +0200

    class created (NOT DONE)

---
+++ classForJokes.js
@@ -0,0 +1,10 @@
+export function classForJokes(resource) {
+    console.log(resource);
+    
+    class Joke {
+        constructor(data) {
+            this.status = data.status;
+        }
+        
+    }
+}
